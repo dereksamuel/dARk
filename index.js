1e3c6d3ba0bb4519b6653595db5e225f6654191a
Pipaplutarco <dereksamuelgr@gmail.com>
2021-12-16 12:05:33 -0500

    create initARK

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const $root = document.querySelector("canvas#root");
+const ctx = $root.getContext("2d");
+
 const $camera = document.getElementById("camera");
+const $errorVideo = document.getElementById("errorVideo");
 
 const constrains = {
   audio: true,
@@ -9,11 +13,30 @@ const constrains = {
   }
 };
 
+// {
+//   facingMode: {
+//     exact: "environment"
+//   }
+// }
+
+function initARK() {
+  ctx.drawImage($camera, 0, 0, $root.width, $root.height);
+
+  requestAnimationFrame(initARK);
+}
+
 navigator.mediaDevices.getUserMedia(constrains)
   .then((stream) => {
     $camera.srcObject = stream;
     $camera.play();
+
+    $camera.onloadedmetadata = () => {
+      initARK();
+    };
   })
   .catch((error) => {
     console.error("An error ocurred on started camera", error);
+
+    $errorVideo.classList.remove("hidde");
+    $errorVideo.classList.add("show");
   });
